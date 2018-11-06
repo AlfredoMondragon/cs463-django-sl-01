3168fea2129f8019f5f47d7b3931a78c6af13dc9
AlfredoMondragon <amondragon4@live.nmhu.edu>
2018-11-06 12:09:12 -0700

    Add files via upload

--- shopper_app/views.py
+++ shopper_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
 # generic views docs: https://docs.djangoproject.com/en/2.1/ref/class-based-views/
 from django.views import generic 
-
 from .models import Item
 
 
